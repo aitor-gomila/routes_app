0e3882ff5ed772ec6825136d83cdbf83441e4c22
aitor-gomila <aitor-gomila@outlook.com>
2023-05-29 17:08:26 +0200

    Change application name

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class Application extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "GPS Routes",
+      title: "Routes App",
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
@@ -59,7 +59,7 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text("GPS Routes"),
+        title: const Text("Routes App"),
       ),
       body: RoutesList(onItemPressed: _onItemPressed)
     );
